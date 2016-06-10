7ab2366047f15dd703d1ded61fe9baf33dc0fd38
Justin Durham <justin.durham@icloud.com>
2016-06-10 08:48:16 -0400

    Updates

---
+++ test/integration/index.integration.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import Redis from '../../src/index';
+
+const redis = new Redis({
+	host: '192.168.99.100',
+	port: 32769
+});
+
+describe('#smembers()', () => {
+	it('should return empty array', (done) => {
+		redis.smembers('nokey', (err, result) => {
+			result.should.be.an.Array;
+			result.length.should.be.equal(0);
+			done(err);
+		});
+	});
+});
