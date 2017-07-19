95a2323452be8b029b1899945ac1d6a818c26be2
Justin Durham <justin.durham@icloud.com>
2017-07-19 13:22:10 -0400

    Update types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Client from './client';
+import Batch from './batch';
+import Transaction from './transaction';
 
 export default Client;
 
-export { Client };
+export { Client, Batch, Transaction };
